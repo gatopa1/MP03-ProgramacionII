c29983d9480ab461c2d4061937e6e55f8cf9c25c
gatopa1 <sergiojg2@yahoo.es>
2016-09-19 10:04:05 +0200

    Update holamundo.java

--- holamundo.java
+++ holamundo.java
@@ -11,7 +11,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        System.out.println("hola a todos");
     }
     
 }
