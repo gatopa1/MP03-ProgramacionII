caed94dd2e358d46d7d544ca6a917b433554c1b3
gatopa1 <sergiojg2@yahoo.es>
2016-09-19 09:47:40 +0200

    version5

---
+++ holamundo.java
@@ -0,0 +1,17 @@
+package javaapplication2;
+
+/**
+ *
+ * @author ser
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");
+    }
+    
+}
